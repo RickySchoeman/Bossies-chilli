3f3d08677cb4bec3abe5579b82a0eed5bd73824c
ricky schoeman <schoeman.ricky@gmail.com>
2021-01-19 18:23:34 +0100

    issues with session?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,6 @@
 class OrdersController < ApplicationController
 
   def create
-<<<<<<< HEAD
-  sauce = Sauce.find(params[:sauce_id])
-  order  = Order.create!(sauce: sauce, amount: sauce.price, state: 'pending')
-  redirect_to order_path(order)
-end
-
-def show
-  @order = Order.find(params[:id])
-end
-=======
     sauce = Sauce.find(params[:sauce_id])
     order  = Order.create!(sauce: sauce, amount: sauce.price, state: 'pending')
   end
@@ -21,14 +11,13 @@ end
 
   def edit
     @order = Order.find(params[:id])
-    # update the order
-    # redirect to order show
+     update the order
+     redirect to order show
   end
 
 
   def show
     @order = Order.find(params[:id])
   end
->>>>>>> e9907f280838b5a4a4a35bbef5f1617540621854
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :sauces, only: [:index, :show]
   resources :order_sauces, only: [:create]
-  resources :orders, only: [:show, :create] do
+  resources :orders, only: [:show, :create, :update, :edit] do
     resources :payments, only: :new
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
