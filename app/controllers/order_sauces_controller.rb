a37ca09aec22cb03264549e56d5661c32ec89e87
ricky schoeman <schoeman.ricky@gmail.com>
2021-01-19 20:28:13 +0100

    changed .present to. blank, made all the difference

--- app/controllers/order_sauces_controller.rb
+++ app/controllers/order_sauces_controller.rb
@@ -1,7 +1,7 @@
 class OrderSaucesController < ApplicationController
 
   def create
-    if params[:order_sauce][:order_id].present?
+    if params[:order_sauce][:order_id].blank?
       order = Order.create(state: 'pending')
       session[:order_id] = order.id
       order_sauce = OrderSauce.new(order_sauce_params)
