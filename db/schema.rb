2b9dfb6e89e493bd8a67e777444e97ed7f80495a
ricky schoeman <schoeman.ricky@gmail.com>
2021-02-16 10:40:43 +0100

    properly added user migration :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_115145) do
+ActiveRecord::Schema.define(version: 2021_02_16_093146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
